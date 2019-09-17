b241ebf9e5b2b3388c222408dd9a7ab873d4c853
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-17 09:51:54 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+//opgave 3
+    public static void main(String[] args) {
+	// kode til opgave 3
+        int first = 30;
+        int second = 67;
+
+        //test til 3
+        if (first >second)
+            System.out.println("first er større end second");
+        else System.out.println("second er større end first");
+
+
+
+
+
+    }
+}
